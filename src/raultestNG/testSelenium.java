98787315b08a9164b8583b1f23e613a0aabed40e
taryan <taryan@LS24556.partners.daatagroup.com>
2019-11-02 13:24:55 -0400

    I am testing GitHub 0124

--- src/raultestNG/testSelenium.java
+++ src/raultestNG/testSelenium.java
@@ -4,8 +4,9 @@ public class testSelenium {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int test=1111;
-		System.out.println("TEST"+test);
+		int test=2222;
+		int b= 1212
+		System.out.println("TEST"+test+b);
 		int a=200;
 		
 		
